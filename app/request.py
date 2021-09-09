1864f7d55715d8e9b65ca0bb9f1083f2829a4de3
ken <ken.mbira@student.moringaschool.com>
2021-09-09 15:50:37 +0300

    add a url lib request and json interpreter

--- app/request.py
+++ app/request.py
@@ -1,3 +1,13 @@
 from app import app
+import urllib.request,json
+from .models import book
+
+Book = book.Book
 
 key = app.config['key']
+base_url = app.config["BOOK_API_BASE_URL"]
+
+def get_books(category):
+    """
+    This function will take in a book genre and return all books that are found in the specific genre
+    """
